333af8d4824d912153dbbf51bfd8821dd090fa7a
Danh Vo <dqvo13@gmail.com>
2022-06-10 13:15:26 -0700

    initial commit

---
+++ public/scripts/studentDataAddNav.js
@@ -0,0 +1,5 @@
+window.onload = function() {
+    document.getElementById("nav-studHome").addEventListener("click", function () {
+        window.location.assign("./studentDataHome.html");
+    }, false);
+}
---
+++ public/scripts/studentDataHomeNav.js
@@ -0,0 +1,5 @@
+window.onload = function() {
+    document.getElementById("nav-addStudent").addEventListener("click", function () {
+        window.location.assign("./studentDataAdd.html");
+    }, false);
+}
