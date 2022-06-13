0d1a9c92fae73b242c2b01b4a00cbb14f51644bf
dqvo13 <dqvo13@gmail.com>
2022-06-12 22:46:47 -0700

    added post util to index

--- index.js
+++ index.js
@@ -2,9 +2,26 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app = express()
+
+// understand JSON
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+app.get('/', (req, res) => res.render('pages/index'))
+app.post('/public/studentDataAdd.html', (req, res) => {
+  var db_studID = req.body.f_studID  
+  var db_firstName = req.body.f_firstName
+  var db_lastName = req.body.f_lastName
+  var db_gpa = req.body.f_gpa
+  var db_age = req.body.f_age
+  var db_height = req.body.f_height
+  var db_weight = req.body.f_weight
+  var db_hairColour = req.body.f_hairColour
+
+  
+})
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
