39b46225277252963c4e72cdd2cd641ad2677746
dqvo13 <dqvo13@gmail.com>
2022-06-14 19:01:16 -0700

    moved static pages into views, working on routing

---
+++ README.txt
@@ -0,0 +1,3 @@
+An interactive student database made for Assignment 2 of CMPT 276, Summer 2022.
+App deployed by Heroku, which uses PostgreSQL for database management. 
+Built using HTML, CSS, Javascript, with Bootstrap and NodeJS frameworks. 
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
@@ -7,27 +8,41 @@ const { Pool } = require('pg');
 var pool = new Pool({
   // localhost server
   // connectionString: 'schema://user:password@host/database'
-  // connectionString: 'postgres://postgres:adm1n-superroot@localhost/students'
+  connectionString: 'postgres://postgres:adm1n-superroot@localhost/students'
 
-  // take database variable given by heroku
-  connectionString: process.env.DATABASE_URL,
+  // heroku server
+  
+  // connectionString: process.env.DATABASE_URL
+  /*
   ssl: {
     rejectUnauthorized: false
   }
+  */
 });
-pool.connect();
+// pool.connect()
 
-app = express()
+app = express();
 
 // understand JSON
-app.use(express.json())
-app.use(express.urlencoded({extended:false}))
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
 
 // basic routing
-app.use(express.static(path.join(__dirname, 'public')))
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
-app.get('/', (req, res) => res.render('pages/index'))
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.get('/', (req, res) => {
+  var getStudentsQuery = `SELECT * FROM studData`;
+  pool.query(getStudentsQuery, (err, result) => {
+    if (err) res.end(err);                  // end response if there's an error
+    var tableObj = {'rows':result.rows};    // JSON object containing results of query
+    res.render('pages/studentDataHome', tableObj);
+  })
+});
+app.route('/navAdd').get((req, res) => {
+  console.log("going to add page...")
+  res.render('pages/studentDataAdd')
+})
 
 // postgreSQL usages
 app.get('/viewStudents', (req, res) => {
@@ -38,6 +53,7 @@ app.get('/viewStudents', (req, res) => {
     res.render('pages/db', tableObj);
   })
 })
+/*
 app.get('/student/:id', (req, res) => {
   var studID = req.params.id;
   console.log("looking for student with ID " + studID);
@@ -51,6 +67,7 @@ app.get('/student/:id', (req, res) => {
     res.render('pages/db', tableObj);
   })
 })
+*/
 app.post('/addStudent', (req, res) => {
   var db_studID = req.body.f_studID  
   var db_firstName = req.body.f_firstName
