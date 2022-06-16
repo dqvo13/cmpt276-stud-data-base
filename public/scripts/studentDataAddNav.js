b7f9fb398a121be3c22e9d5b552a73bc108ed8be
dqvo13 <dqvo13@gmail.com>
2022-06-15 23:52:31 -0700

    cleaning up file tree

--- public/scripts/studentDataAddNav.js
+++
@@ -1,5 +0,0 @@
-window.onload = function() {
-    document.getElementById("nav-studHome").addEventListener("click", function () {
-        window.location.assign("./studentDataHome.html");
-    }, false);
-}
